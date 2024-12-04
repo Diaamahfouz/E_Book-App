61b109a7185f01e80f99bc294d8a52f2e7005213
diaa mahfouz <mahfouzdiaa5@gmail.com>
2024-12-04 03:20:38 +0200

    add font family to our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,24 @@ import 'package:e_book/constants/constants.dart';
 import 'package:e_book/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const EBookApp());
 }
+
 class EBookApp extends StatelessWidget {
   const EBookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor:kPrimarycolor),
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimarycolor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       debugShowCheckedModeBanner: false,
-      home:const SplashView(),
+      home: const SplashView(),
     );
   }
 }
