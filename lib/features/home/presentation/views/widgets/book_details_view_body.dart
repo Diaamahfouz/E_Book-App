d9f76c3ba5e2fe94b68ebc4a75fb59f1b38dd40f
diaa mahfouz <mahfouzdiaa5@gmail.com>
2024-12-18 20:36:40 +0200

    create search view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,5 @@
-import 'package:e_book/core/utils/styles.dart';
 import 'package:e_book/features/home/presentation/views/widgets/book_details_section.dart';
-
 import 'package:e_book/features/home/presentation/views/widgets/custom_app_bar_book_details.dart';
-
-import 'package:e_book/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:e_book/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
